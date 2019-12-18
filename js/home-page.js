da0211c65cb59af459e76db96f6093a85c326f14
Ksenia <kondrashova.ksenya@gmail.com>
2019-12-17 19:47:37 -0500

    polish

--- js/home-page.js
+++ js/home-page.js
@@ -2,12 +2,19 @@
 const btns = Array.from(document.querySelectorAll(".btn"));
 btns.forEach(function (b) {
     b.onclick = function (e) {
+        console.log(e.target);
         if (e.target.classList.contains("what-I-do")) {
             window.location.href="../what-I-do.html";
         } else if (e.target.classList.contains("who-I-am")) {
             window.location.href = "../who-I-am.html";
         } else if (e.target.classList.contains("hire-me")) {
             window.location.href = "../hire-me.html";
+        } else if (e.target.classList.contains("instagram") || e.target.parentNode.classList.contains("instagram")) {
+            window.location.href = "https://www.instagram.com/ksenia_it_is/";
+        } else if (e.target.classList.contains("upwork") || e.target.parentNode.classList.contains("upwork")) {
+            window.location.href = "https://www.upwork.com/o/profiles/users/_~01d7272896b74a4413/";
+        } else if (e.target.classList.contains("linkedIn") || e.target.parentNode.classList.contains("linkedIn")) {
+            window.location.href = "https://www.linkedin.com/in/ksenia-kondrashova/";
         }
     }
 });
