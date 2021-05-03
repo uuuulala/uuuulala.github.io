6ecfb762f82cf9815bb6da289f343db957843cce
Ksenia Kondrashova <kondrashova.ksenya@gmail.com>
2021-05-03 15:41:44 +0300

    wave animation deleted, some back shader tweaks

--- js/back-animation.js
+++ js/back-animation.js
@@ -8,7 +8,6 @@ function initHeroAnimation(canvas) {
         VELOCITY_DISSIPATION: 0.8,
         PRESSURE: 0.5,
         PRESSURE_ITERATIONS: 40,
-        CURL: 0,
         SPLAT_RADIUS: 0.01,
     };
 
@@ -356,32 +355,30 @@ function initHeroAnimation(canvas) {
         }
     `);
 
-    const blit = (() => {
+    function blit(target) {
         gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([-1, -1, -1, 1, 1, 1, 1, -1]), gl.STATIC_DRAW);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+            -1, -1,
+            -1, 1,
+            1, 1,
+            1, -1
+        ]), gl.STATIC_DRAW);
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, gl.createBuffer());
         gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array([0, 1, 2, 0, 2, 3]), gl.STATIC_DRAW);
         gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(0);
 
-        return (target, clear = false) => {
-            if (target == null) {
-                gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
-                gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-            }
-            else {
-                gl.viewport(0, 0, target.width, target.height);
-                gl.bindFramebuffer(gl.FRAMEBUFFER, target.fbo);
-            }
-            if (clear) {
-                gl.clearColor(0.0, 0.0, 0.0, 1.0);
-                gl.clear(gl.COLOR_BUFFER_BIT);
-            }
-            gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
+        if (target == null) {
+            gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+            gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+        } else {
+            gl.viewport(0, 0, target.width, target.height);
+            gl.bindFramebuffer(gl.FRAMEBUFFER, target.fbo);
         }
-    })();
+        gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
+    }
 
-    let dye, velocity, divergence, curl, pressure;
+    let dye, velocity, divergence, pressure;
 
     const copyProgram = new Program(baseVertexShader, copyShader);
     const blurProgram = new Program(baseVertexShader, blurShader);
@@ -417,7 +414,6 @@ function initHeroAnimation(canvas) {
             velocity = resizeDoubleFBO(velocity, simRes.width, simRes.height, rg.internalFormat, rg.format, texType, filtering);
 
         divergence = createFBO(simRes.width, simRes.height, r.internalFormat, r.format, texType, gl.NEAREST);
-        curl = createFBO(simRes.width, simRes.height, r.internalFormat, r.format, texType, gl.NEAREST);
         pressure = createDoubleFBO(simRes.width, simRes.height, r.internalFormat, r.format, texType, gl.NEAREST);
     }
 
