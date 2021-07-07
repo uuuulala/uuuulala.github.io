24a725632a05f076a291dbb3e33d5840276957ea
Ksenia Kondrashova <kondrashova.ksenya@gmail.com>
2021-07-07 15:02:58 +0400

    gooey try to pin

--- js/gooey.js
+++ js/gooey.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
     gsap.timeline({
         scrollTrigger: {
             trigger: gooeyContainer,
+            pin: window.innerWidth < 800,
             onEnter: () => {
                 gooeySurface.isVisible = true;
                 gooeySurface.loop();
